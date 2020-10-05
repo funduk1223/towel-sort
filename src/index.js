0971bdae84df2706168179d463e3179535b190de
Mikhail Akhmedzyanov <mikhail.akhmedzyanov@gmail.com>
2020-10-05 13:30:01 +0300

    feat: done towel-sort task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,35 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+
+  function compareDownToUp(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }
+
+  function compareUpToDown(a, b) {
+    if (a > b) return -1;
+    if (a == b) return 0;
+    if (a < b) return 1;
+  }
+  if (matrix === undefined)
   return [];
+  else{
+    let order = 0;
+    let newArr = [];
+    matrix.forEach(function(row){
+      row.sort(compareDownToUp);
+      if (order % 2 !== 0){
+        row.reverse();
+      }
+      row.forEach(function(num){
+        newArr.push(num);
+      });
+      order++;
+    });
+    return newArr;
+  }
+
+  
 }
